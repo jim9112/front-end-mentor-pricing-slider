72141cd20060212b677f70745412d8c0716725bc
jim9112 <jim9112@gmail.com>
2021-01-24 21:48:55 -0500

    footer and price styling

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const slider = document.querySelector('.slider');
 const pageViews = document.querySelector('.page-views');
+const cost = document.querySelector('.cost');
+const price = parseFloat(slider.value) * .10
+
+
+
+const moneyOutputFromString = (string) => {
+    const newPrice = parseFloat(string) * .10;
+    const moneyOutput = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(newPrice);
+    return moneyOutput;
+}
+
+const handleSlider = (e) => {
+    const sliderValue = e.currentTarget.value;
+    pageViews.textContent = sliderValue;
+    cost.textContent = moneyOutputFromString(sliderValue);
+}
+
+slider.addEventListener('change', handleSlider)
+
 pageViews.textContent = slider.value;
-slider.addEventListener('change', (e) => pageViews.textContent = e.currentTarget.value)
+cost.textContent = moneyOutputFromString(slider.value);
